bce0b6ccee06630a04c5590caf0b4ed42b359c90
GiacomoCrevani <89921268+GiacomoCrevani@users.noreply.github.com>
2021-11-24 13:16:02 +0100

    Update mintohours_norm.py

--- mintohours_norm.py
+++ mintohours_norm.py
@@ -15,12 +15,12 @@ minute_index = pd.date_range("2021-01-01 00:00:00", "2021-12-31 23:59:00", freq=
 hour_index = np.linspace(1,8760,8760,dtype=int)
 
 #from load to loadH 
-load=pd.read_csv('C:/Users/giaco/OneDrive - Politecnico di Milano/Crevani - CESP/Project with Gommyr/RAMP-master/ramp/results/output_file_3.csv',usecols=['0'])
+load=pd.read_csv('#INSERT THE PATH TO THE .csv FILE FROM RAMP OUTPUT#',usecols=['0'])
 load.index = minute_index
 loadH = load.resample('H').mean() 
 loadH.index = hour_index
 
 #export in .csv 
-loadH.to_csv('hh_only.csv')
+loadH.to_csv('#XXX.csv TO NAME THE FILE OF OUTPUT IN HOURLY RESOLUTION#')
 
 
