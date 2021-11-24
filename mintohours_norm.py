a79268e3ed60d77602407b56b404ff40fb98e2b1
GiacomoCrevani <89921268+GiacomoCrevani@users.noreply.github.com>
2021-11-24 13:09:55 +0100

    Add files via upload

---
+++ mintohours_norm.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  10 10:18:55 2021
+@author: giaco
+"""
+
+#code useful to shift from min resolution to hourly, such that the csv load demand can be fed as input to MicrogridsPy
+#yearly profile is required
+
+import numpy as np
+import pandas as pd
+
+#minute and hour indexes
+minute_index = pd.date_range("2021-01-01 00:00:00", "2021-12-31 23:59:00", freq="1min")
+hour_index = np.linspace(1,8760,8760,dtype=int)
+
+#from load to loadH 
+load=pd.read_csv('C:/Users/giaco/OneDrive - Politecnico di Milano/Crevani - CESP/Project with Gommyr/RAMP-master/ramp/results/output_file_3.csv',usecols=['0'])
+load.index = minute_index
+loadH = load.resample('H').mean() 
+loadH.index = hour_index
+
+#export in .csv 
+loadH.to_csv('hh_only.csv')
+
+
